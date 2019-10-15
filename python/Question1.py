d42071688e669f9fa2631f49627fb89c8d8d65a9
Atul kumar Agrawal <ms.atul1303@gmail.com>
2019-10-15 23:30:56 +0530

    Update q1.py

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,23 @@
 # modify this function, and create other functions below as you wish
-def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
+#def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
     # modify and then return the variable below
-    answer = -5
+#    answer = -5
+#    return answer
+def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
+    initialLevelOfDebt=float(initialLevelOfDebt)
+    interestPercentage=float(interestPercentage)
+    repaymentPercentage=float(repaymentPercentage)
+    #print(initialLevelOfDebt)
+    #print(interestPercentage)
+    #print(repaymentPercentage)
+    rem=float(initialLevelOfDebt)
+    i=int(0)
+    x=(repaymentPercentage*initialLevelOfDebt/100)
+    while rem>50:
+    	rem=(rem+(interestPercentage/100)*rem-(x))
+    	#print(rem)
+    	i=i+1
+    	#print("i="+str(i))
+    
+    answer =int(i*(x)+(0.1*initialLevelOfDebt)+rem)
     return answer
